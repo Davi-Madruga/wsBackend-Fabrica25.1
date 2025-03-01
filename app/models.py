748dc88977787249431aaf62d489449f04abadeb
Davi Madruga <davimadruga@outlook.com.br>
2025-03-01 09:46:29 -0300

    Feat: Criação do Modelo Usuario, com seus atributos

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    nome = models.CharField(max_length=30)
+    email = models.EmailField(max_length=30)
+    senha = models.CharField(max_length=15)
+    data_nascimento = models.DateField()
+    cpf = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=11)
+    
+    def __str__(self):
+        return self.nome
