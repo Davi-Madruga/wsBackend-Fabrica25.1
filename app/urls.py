8356aaa96570d5dfbf00782e7518e8f026e09e99
Davi Madruga <davimadruga@outlook.com.br>
2025-03-01 10:16:42 -0300

    Registrando o nome do app

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
 
+app_name = 'app'
+
 urlpatterns = [
     path('criar/', criar_view, name='criar'),
     path('listar/', listar_view, name='listar'),
