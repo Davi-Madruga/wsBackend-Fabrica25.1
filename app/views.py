986d5db8a4df3955b35d21c116a73247bd6dace2
Davi Madruga <davimadruga@outlook.com.br>
2025-03-01 10:59:35 -0300

    Feat: Atualização da função editar_view
    Agora a função editar_view ira coletar o id(pk) do usuário e o forms respondido e enviar para o editar.html. lá o usuário poderá editar suas informações e retorna-lás.

--- app/views.py
+++ app/views.py
@@ -16,7 +16,17 @@ def listar_view(request):
     usuarios = Usuario.objects.all()
     return render(request, 'app/listar.html', {'usuarios': usuarios})
 
-def editar_view(request):
+def editar_view(request, pk):
+    usuario = Usuario.objects.get(pk=pk)
+    if request.method == 'GET':
+        form = UsuarioForm(instance=usuario)
+        return render(request, 'app/editar.html', {'form': form, 'usuario': usuario})
+    elif request.method == 'POST':
+        form = UsuarioForm(request.POST, instance=usuario)
+        if form.is_valid():
+            form.save()
+            return redirect('app:listar')
+        
     return render(request, 'editar.html')
 
 def deletar_view(request):
