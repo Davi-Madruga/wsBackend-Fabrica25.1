c6dcf51769576b7f4605602c8437f3d51a5a8aa4
Davi Madruga <davimadruga@outlook.com.br>
2025-03-01 10:01:01 -0300

    Feat: Criação dos paths das views já criadas

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
-    
+    path('criar/', criar_view, name='criar'),
+    path('listar/', listar_view, name='listar'),
+    path('editar/', editar_view, name='editar'),
+    path('deletar/', deletar_view, name='deletar'),
+    path('detalhar/', detalhar_view, name='detalhar'),
 ]
