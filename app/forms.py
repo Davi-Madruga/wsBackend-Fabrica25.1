3d6088cabefb336e7fe1f291de1ecb28a7ca11ea
Davi Madruga <davimadruga@outlook.com.br>
2025-03-01 09:53:06 -0300

    Feat: Criação do forms.py, e config do UsuarioForm

---
+++ app/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Usuario
+
+class UsuarioForm(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = ['nome', 'email', 'senha', 'data_nascimento', 'cpf', 'telefone']
+        widgets = {
+            'nome': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'senha': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'data_nascimento': forms.DateInput(attrs={'class': 'form-control'}),
+            'cpf': forms.TextInput(attrs={'class': 'form-control'}),
+            'telefone': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'nome': 'Nome',
+            'email': 'Email',
+            'senha': 'Senha',
+            'data_nascimento': 'Data de Nascimento',
+            'cpf': 'CPF',
+            'telefone': 'Telefone',
+        }
+        
