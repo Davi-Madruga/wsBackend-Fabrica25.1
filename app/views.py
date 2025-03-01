7a778e0812ee39798e184cbc99503227fe4ca155
Davi Madruga <davimadruga@outlook.com.br>
2025-03-01 09:59:42 -0300

    Update views.py, adição de futuras funções

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+def criar_view(request):
+    return render(request, 'criar.html')
+
+def listar_view(request):
+    return render(request, 'listar.html')
+
+def editar_view(request):
+    return render(request, 'editar.html')
+
+def deletar_view(request):
+    return render(request, 'deletar.html')
+
+def detalhar_view(request):
+    return render(request, 'detalhar.html')
